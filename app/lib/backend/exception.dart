139152c10d7593c243f73294b8db4c5b82de95a8
candide guevara <candide-guevara@users.noreply.github.com>
2025-01-11 15:04:26 -0500

    fix problem persisting empty lists

---
+++ app/lib/backend/exception.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+
+class ExceptionAndStack implements Exception {
+  final String msg;
+  final Object err;
+  final StackTrace trace;
+  ExceptionAndStack(this.msg, this.err, this.trace);
+  String toString() {
+    return "${msg}: ${err}\n${framesStr()}";
+  }
+  String framesStr() {
+    final List<StackFrame> frames = StackFrame.fromStackTrace(trace);
+    return frames
+      .skipWhile((s) => !s.source.contains('package:app'))
+      .take(3)
+      .map((s) => "${s.source}").join("\n");
+  }
+}
+
--- app/lib/backend/gender_game_config.dart
+++ app/lib/backend/gender_game_config.dart
@@ -1,28 +1,59 @@
 import 'utils.dart';
+import 'exception.dart';
 import 'persistence_store.dart';
 
 class GenderGameConfig {
-  final int word_cnt;
-  final int min_freq;
-  final List<TagType> exclude_tags;
+  int word_cnt;
+  int min_freq;
+  Set<TagType> exclude_tags;
 
-  GenderGameConfig(this.word_cnt, this.min_freq, this.exclude_tags);
+  GenderGameConfig(this.word_cnt, this.min_freq, List<TagType> tags)
+    : exclude_tags = Set<TagType>.of(tags);
 
-  static GenderGameConfig load() {
+  GenderGameConfig.def(): this(20, 2, []);
+
+  bool has(TagType t) => exclude_tags.contains(t);
+  void set(TagType t) => exclude_tags.add(t);
+
+  static Future<GenderGameConfig> load() async {
     final s = Persistence.store;
-    return GenderGameConfig(
-      s.getInt('GenderGameConfig_word_cnt') ?? 10,
-      s.getInt('GenderGameConfig_min_freq') ?? 2,
-      s.getFromInts<TagType>('GenderGameConfig_exclude_tags',
-                             (i) => TagType.values[i]) ?? [],
-    );
+    final def = GenderGameConfig.def();
+    try{
+      final cnt =  await s.getInt('GenderGameConfig_word_cnt') ?? def.word_cnt;
+      final frq =  await s.getInt('GenderGameConfig_min_freq') ?? def.min_freq;
+      final tag =  await s.getFromInts<TagType>('GenderGameConfig_exclude_tags',
+                             (i) => TagType.values[i]) ?? def.exclude_tags.toList();
+      return GenderGameConfig(cnt, frq, tag);
+    }
+    catch(e,st) {
+      throw ExceptionAndStack("GenderGameConfig.load failed", e, st);
+    }
   }
-  Future<void> save() {
+  Future<void> save() async {
     final s = Persistence.store;
-    return s.setInt('GenderGameConfig_word_cnt', word_cnt)
-            .then((_) => s.setInt('GenderGameConfig_min_freq', min_freq))
-            .then((_) => s.setIntList('GenderGameConfig_exclude_tags',
-                                      exclude_tags.map((e) => e.index).toList()));
+    try{
+      await s.setInt('GenderGameConfig_word_cnt', word_cnt);
+      await s.setInt('GenderGameConfig_min_freq', min_freq);
+      // Saving an empty list causes a problem when unmarshalling.
+      if (exclude_tags.isNotEmpty) {
+        await s.setIntList('GenderGameConfig_exclude_tags',
+                           exclude_tags.map((e) => e.index).toList());
+      }
+      else {
+        await s.remove('GenderGameConfig_exclude_tags');
+      }
+    }
+    catch(e,st) {
+      throw ExceptionAndStack("GenderGameConfig.save failed", e, st);
+    }
+  }
+
+  String toString() {
+    return """
+    word_cnt: ${word_cnt}
+    min_freq: ${min_freq}
+    exclude_tags: ${exclude_tags.map((e) => e.toString()).toList()}
+    """;
   }
 }
 
