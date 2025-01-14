bd3796038e627280ecd10a8f8c167bf5ce6d1dce
candide guevara <candide-guevara@users.noreply.github.com>
2025-01-13 23:04:17 -0500

    GenderGameHistory to and from json

--- app/lib/backend/gender_game_state.dart
+++ app/lib/backend/gender_game_state.dart
@@ -1,6 +1,31 @@
 import 'dictionary.dart';
 import 'dictionary_entry.dart';
-import 'persistence_store.dart';
+
+class PastGame {
+  final DateTime date;
+  final int good;
+  final int bad;
+  PastGame(this.date, this.good, this.bad);
+
+  PastGame.fromJson(Map<String, dynamic> json):
+    date = DateTime.fromMillisecondsSinceEpoch(json['date']),
+    good = json['good'],
+    bad  = json['bad'];
+
+  Map<String, dynamic> toJson() => {
+    'date' : date.millisecondsSinceEpoch,
+    'good' : good,
+    'bad'  : bad,
+  };
+
+  String toString() {
+    final buf = StringBuffer();
+    buf.writeln("date: ${date},");
+    buf.writeln("good: ${good},");
+    buf.writeln("bad: ${bad},");
+    return buf.toString();
+  }
+}
 
 class GenderGameState {
   final DateTime date;
@@ -15,6 +40,8 @@ class GenderGameState {
     if(correct) { good.add(entry); }
     else { good.add(entry); }
   }
+
+  PastGame done() { return PastGame(date, good.length, bad.length); }
 }
 
 class HistoryEntry {
@@ -22,6 +49,8 @@ class HistoryEntry {
   final List<DateTime> fails;
   final String word;
   final int meaning_idx;
+
+  HistoryEntry.empty(): goods = [], fails = [], word = '', meaning_idx = 0;
 
   HistoryEntry.fromJson(Map<String, dynamic> json):
     goods = [ for(final d in json['goods'] ?? []) DateTime.fromMillisecondsSinceEpoch(d) ],
@@ -48,16 +77,27 @@ class HistoryEntry {
   }
 }
 
-class PastGame {
-  final DateTime date;
-  final int good;
-  final int bad;
-  PastGame(this.date, this.good, this.bad);
-}
-
 class GenderGameHistory {
   List<HistoryEntry> history;
   List<PastGame> past_games;
   GenderGameHistory.empty(): history = [], past_games = [];
+
+  GenderGameHistory.fromJson(Map<String, dynamic> json):
+    history = (json?['history'] ?? []).map<HistoryEntry>((d) => HistoryEntry.fromJson(d)).toList(),
+    past_games = (json?['past_games'] ?? []).map<PastGame>((d) => PastGame.fromJson(d)).toList();
+
+  Map<String, dynamic> toJson() => {
+    'history' :  [ for(final h in history) h.toJson() ],
+    'past_games': [ for(final p in past_games) p.toJson() ],
+  };
+
+  String toString() {
+    final buf = StringBuffer();
+    buf.writeln("## history");
+    history.take(3).forEach(buf.writeln);
+    buf.writeln("## past_games");
+    past_games.take(3).forEach(buf.writeln);
+    return buf.toString();
+  }
 }
 
--- app/test/backend/gender_game_state_test.dart
+++ app/test/backend/gender_game_state_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
-import 'package:test/test.dart';
 import 'package:app/backend/gender_game_state.dart';
+import 'package:test/test.dart';
+import 'package:matcher/expect.dart';
 import 'utils.dart';
 
 void main() {
@@ -26,6 +27,34 @@ void main() {
     final entry = HistoryEntry.fromJson(json.decode(ini_json));
     final new_json = json.encode(entry.toJson());
     compareJsonStr(new_json, ini_json);
+  });
+
+  test('PastGame_fromJson_and_toJson', () {
+    final pg = PastGame(DateTime(2020, 12, 12), 6, 7);
+    final jsonObj = pg.toJson();
+    final jsonStr = json.encode(jsonObj);
+    final new_pg = PastGame.fromJson(json.decode(jsonStr));
+    expect(new_pg.toString(), equals(pg.toString()));
+  });
+
+  test('GenderGameHistory_fromJson_and_toJson', () {
+    final ggh = GenderGameHistory.empty();
+    ggh.history.add(HistoryEntry.empty());
+    ggh.past_games.add(PastGame(DateTime(2020, 12, 12), 6, 7));
+    final jsonObj = ggh.toJson();
+    final jsonStr = json.encode(jsonObj);
+    final new_ggh = GenderGameHistory.fromJson(json.decode(jsonStr));
+    // BE CAREFUL IT IS A TRAP!
+    // `expect` fails to compare normal objects...
+    expect(new_ggh.toString(), equals(ggh.toString()));
+  });
+
+  test('GenderGameHistory_fromJson_and_toJson_empty', () {
+    final ggh = GenderGameHistory.empty();
+    final jsonObj = ggh.toJson();
+    final jsonStr = json.encode(jsonObj);
+    final new_ggh = GenderGameHistory.fromJson(json.decode(jsonStr));
+    expect(new_ggh.toString(), equals(ggh.toString()));
   });
 }
 
