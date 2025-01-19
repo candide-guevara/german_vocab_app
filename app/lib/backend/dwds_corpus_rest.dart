1f5d5911c2bbd3bbd00a3a0dcff47bae66ab4b97
candide guevara <candide-guevara@users.noreply.github.com>
2025-01-19 01:50:51 +0100

    parse corpus entries using json format

--- app/lib/backend/dwds_corpus_rest.dart
+++ app/lib/backend/dwds_corpus_rest.dart
@@ -3,21 +3,25 @@ import 'dart:convert';
 import 'dart:isolate';
 
 final List<String> kUrlRoots = [
-  'https://www.dwds.de/r/?view=tsv&corpus=kern21&genre=Zeitung&q=',
-  'https://www.dwds.de/r/?view=tsv&corpus=kern&genre=Zeitung&q=',
-  'https://www.dwds.de/r/?view=tsv&corpus=kern21&q=',
-  'https://www.dwds.de/r/?view=tsv&corpus=kern&q=',
+  'https://www.dwds.de/r/?corpus=dwdsxl&sc=bz&sc=blogs&sc=tsp&sc=wikipedia&format=full&sort=date_desc&limit=30&view=json&q=',
+  'https://www.dwds.de/r/?corpus=dwdsxl&sc=adg&sc=kern&sc=kern21&sc=wikibooks&format=full&sort=date_desc&limit=30&view=json&q=',
 ];
+final int kMinCorpusSentences = 5;
 
 class Corpus {
   static final kLineTerm = RegExp(r'(\r?\n)+');
   static final kItemSep = RegExp(r'\t+');
   static final kEmpty = RegExp(r'^\s*$');
+  static final kGermanChars = RegExp(r'[a-zA-ZäöüÄÖÜß0-9 ,\.]');
+  static final int kMinSentenceLen = 64;
+  static final int kMaxSentenceLen = 256;
+  static final double kGermanCharRatio = 0.93;
   final List<String> sentences;
-  Corpus.empty(): sentences = [];
+  final List<(int, int)> token_pos;
+  Corpus.empty(): sentences = [], token_pos = [];
 
   Corpus.fromTsv(final String response, final Uri url)
-      : sentences = [] {
+      : sentences = [], token_pos = [] {
     final lines = response.split(kLineTerm);
     final idx = lines[0].split(kItemSep).indexWhere((tok) {
       return "Hit" == tok;
@@ -33,22 +37,72 @@ class Corpus {
       sentences.add(tokens[idx]);
     }
   }
+
+  static (int, List<String>) collectWords(List<dynamic> jsonList, bool start) {
+    int end = 0;
+    List<String> words = [];
+    const String kSpace = ' ';
+    for (final Map<String, dynamic> o in jsonList) {
+      if (!start && (o['ws'] ?? '0') == '1') {
+        end += 1;
+        words.add(kSpace);
+      }
+      words.add(o['w']!);
+      end += words.last.length + (start?1:0);
+      start = false;
+    }
+    return (end, words);
+  }
+
+  static bool sentenceFilter(final String sentence) {
+    if (sentence.length < kMinSentenceLen) { return false; }
+    if (sentence.length > kMaxSentenceLen) { return false; }
+    final int german_char_cnt = kGermanChars.allMatches(sentence).toList().length;
+    final double ratio = german_char_cnt / sentence.length;
+    if (ratio < kGermanCharRatio) { return false; }
+    return true;
+  }
+  
+  void appendJson(final List<dynamic> response, final Uri url) {
+    for(final Map<String, dynamic> obj in response) {
+      final Map<String, dynamic>? kwic = obj["kwic"];
+      if (kwic == null) { continue; }
+      final (end_ls, ls_words) = collectWords(kwic['ls'] ?? [], true);
+      final (_, rs_words) = collectWords(kwic['rs'] ?? [], end_ls == 0);
+      final (end_kw, kw_words) = collectWords(kwic['kw'] ?? [], false);
+      final String sentence = ls_words.followedBy(kw_words).followedBy(rs_words).join('');
+      if (sentenceFilter(sentence)) {
+        token_pos.add((end_ls, end_ls+end_kw-1));
+        sentences.add(sentence);
+      }
+    }
+  }
+
+  // Empty hits look like this
+  // [{"meta_":{}}]
+  Corpus.fromJson(final List<dynamic> response, final Uri url)
+      : sentences = [], token_pos = [] {
+    appendJson(response, url);
+  }
 }
 
 Future<Corpus> fetchDwdsCorpusFor(final String word) async {
   return Isolate.run(() async {
+    final corpus = Corpus.empty();
     for (final String prefix in kUrlRoots) {
       final url = Uri.parse("${prefix}${word}");
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
         if (response.body.length < 1) { continue; }
-        final corpus = Corpus.fromTsv(response.body, url);
-        if (corpus.sentences.isNotEmpty) { return corpus; }
+        //final corpus = Corpus.fromTsv(response.body, url);
+        final List<dynamic> jsonObj = jsonDecode(response.body);
+        corpus.appendJson(jsonObj, url);
+        if (corpus.sentences.length > kMinCorpusSentences) { return corpus; }
       }
       else { throw Exception("${url} error ${response.statusCode}"); }
     }
-    return Corpus.empty();
+    return corpus;
   });
 }
 
--- app/test/backend/dwds_corpus_rest_test.dart
+++ app/test/backend/dwds_corpus_rest_test.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:german_vocab_app/backend/dwds_corpus_rest.dart';
 import 'package:matcher/expect.dart';
 import 'package:test/test.dart';
@@ -12,6 +13,86 @@ void main() {
     """;
     final corpus = Corpus.fromTsv(rawTsv, Uri.parse("someword"));
     expect(corpus.sentences.length, equals(4));
+  });
+
+  test("Corpus_fromJson", () {
+    final jsonStr = """[
+  {
+    "kwic": {
+      "kw": [
+        { "w": "Kino", "ws": "1", "hl_": 1 }
+      ],
+      "rs": [
+        { "w": "erneut", "ws": "1", "hl_": 0 },
+        { "hl_": 0, "ws": "1", "w": "das" },
+        { "w": "Interesse", "ws": "1", "hl_": 0 },
+        { "hl_": 0, "ws": "1", "w": "an" },
+        { "ws": "1", "w": "der", "hl_": 0 },
+        { "w": "Geschichte", "ws": "1", "hl_": 0 },
+        { "hl_": 0, "ws": "1", "w": "des" },
+        { "hl_": 0, "ws": "1", "w": "Vaterlandes" },
+        { "w": ",", "ws": "0", "hl_": 0 },
+        { "hl_": 0, "ws": "1", "w": "an" },
+        { "hl_": 0, "w": "unseren", "ws": "1" },
+        { "ws": "1", "w": "Wurzeln", "hl_": 0 },
+        { "w": ".", "ws": "0", "hl_": 0 }
+      ],
+      "ls": [
+        { "hl_": 0, "w": "Und", "ws": "1" },
+        { "hl_": 0, "w": "heute", "ws": "1" },
+        { "ws": "1", "w": "wächst", "hl_": 0 },
+        { "ws": "1", "w": "in", "hl_": 0 },
+        { "hl_": 0, "ws": "1", "w": "unserer" },
+        { "hl_": 0, "ws": "1", "w": "Kunst" },
+        { "hl_": 0, "ws": "0", "w": "," },
+        { "ws": "1", "w": "im", "hl_": 0 },
+        { "ws": "1", "w": "Theater", "hl_": 0 },
+        { "ws": "0", "w": ",", "hl_": 0 },
+        { "w": "im", "ws": "1", "hl_": 0 }
+      ]
+    }
+  },
+  {
+    "kwic": {
+      "kw": [
+        { "hl_": 1, "ws": "1", "w": "Kinos" }
+      ],
+      "ls": [
+        { "hl_": 0, "w": "Die", "ws": "1" },
+        { "ws": "1", "w": "Verleiher", "hl_": 0 },
+        { "hl_": 0, "ws": "1", "w": "werden" },
+        { "w": "sich", "ws": "1", "hl_": 0 },
+        { "w": "dieser", "ws": "1", "hl_": 0 },
+        { "ws": "1", "w": "Marktmacht", "hl_": 0 },
+        { "hl_": 0, "w": "gegenüber", "ws": "1" },
+        { "hl_": 0, "ws": "1", "w": "gewogen" },
+        { "w": "zeigen", "ws": "1", "hl_": 0 },
+        { "ws": "1", "w": "müssen", "hl_": 0 },
+        { "hl_": 0, "w": ",", "ws": "0" },
+        { "w": "zuungunsten", "ws": "1", "hl_": 0 },
+        { "w": "anderer", "ws": "1", "hl_": 0 }
+      ],
+      "rs": [
+        { "ws": "0", "w": ",", "hl_": 0 },
+        { "hl_": 0, "ws": "1", "w": "denen" },
+        { "ws": "1", "w": "mancher", "hl_": 0 },
+        { "hl_": 0, "ws": "1", "w": "Film" },
+        { "hl_": 0, "ws": "1", "w": "künftig" },
+        { "w": "vorenthalten", "ws": "1", "hl_": 0 },
+        { "ws": "1", "w": "bleibt", "hl_": 0 },
+        { "hl_": 0, "w": ".", "ws": "0" }
+      ]
+    }
+  }
+]""";
+    final List<String> sentences = [
+      """Und heute wächst in unserer Kunst, im Theater, im Kino erneut das Interesse an der Geschichte des Vaterlandes, an unseren Wurzeln.""",
+      """Die Verleiher werden sich dieser Marktmacht gegenüber gewogen zeigen müssen, zuungunsten anderer Kinos, denen mancher Film künftig vorenthalten bleibt.""",
+    ];
+    final List<dynamic> jsonObj = jsonDecode(jsonStr);
+    final corpus = Corpus.fromJson(jsonObj, Uri.parse("someword"));
+    expect(corpus.sentences, orderedEquals(sentences));
+    expect(corpus.token_pos, orderedEquals([(50,54), (97,102)]));
   });
 }
 
